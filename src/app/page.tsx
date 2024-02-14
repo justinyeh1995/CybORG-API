16e41b46edef8fde4bc353eacd0e17e986ccbf15
justinyeh1995 <justinyeh1995@gmail.com>
2024-02-14 23:32:05 +0000

    start working on client components as well

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import { Inter } from "next/font/google";
+import FileUploader from "@/components/FileUploader";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
