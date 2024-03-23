aeb761bd274a7eb7fec76a750a00f37599e51078
justinyeh1995 <justinyeh1995@gmail.com>
2024-03-23 22:04:48 +0000

    restructure the folder between CybORG & FastAPI as well as Flask

--- src/api/v1/CybORG/CybORG/FastAPI/fastapi_index.py
+++ src/api/v1/CybORG/CybORG/FastAPI/fastapi_index.py
@@ -1,12 +1,16 @@
 from typing import List, Dict, Union
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 
+from CybORG import CybORG, CYBORG_VERSION 
 # @To-Do import CyborgAAS...
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
+    print("Hello FastAPI")
     return {"Hello": "World"}
+
+
