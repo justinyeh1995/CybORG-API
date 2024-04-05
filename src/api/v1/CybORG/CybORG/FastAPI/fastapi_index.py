a6c7a4011dc2f7199b0e4ca5da5cdc548741ba92
justinyeh1995 <justinyeh1995@gmail.com>
2024-04-05 22:33:15 +0000

    update @app.post(/api/game/{game_id}) and add run_all_steps, fix bug in delete game end point

--- src/api/v1/CybORG/CybORG/FastAPI/fastapi_index.py
+++ src/api/v1/CybORG/CybORG/FastAPI/fastapi_index.py
@@ -27,6 +27,7 @@ app.add_middleware(
 # Redis connection # for persistent issue
 r = redis.Redis(host='localhost', port=6379, db=0)
 
+# @To-Do: Better persistence support of an object needed in the future (run it as a subprocess, and store it in queue?)
 # Mapping of game_id to SimpleAgentRunner instances
 active_games = {}
 
@@ -50,8 +51,11 @@ async def start_game(request: Request, config: GameConfig):
 
     game_state = {
         "game_id": game_id,
+        "max_steps": config.steps,
+        "red": config.red_agent,
+        "blue": config.blue_agent,
         "step": 0,
-        "state": runner.game_state_manager.game_states,
+        "state": {},
     }
 
     # Store in Redis
@@ -61,35 +65,55 @@ async def start_game(request: Request, config: GameConfig):
     return {"game_id": game_id}
 
 @app.post("/api/game/{game_id}")
-async def game_step(game_id: str):
+async def run_next_step(game_id: str):
+
+    runner = active_games.get(game_id) 
+    if not runner:
+        raise HTTPException(status_code=404, detail="Game not found")
+
+    game_state_bytes = r.get(game_id)
+    game_state = json.loads(game_state_bytes)
+    
+    state_snapshot = runner.run_next_step()
+    
+    game_state["step"] = runner.current_step
+    game_state["state"] = state_snapshot
+
+    # Store in Redis
+    r.set(game_id, json.dumps(game_state))  # Serialize game_state to store in Redis
+
+    return state_snapshot
+
+@app.post("/api/game/all/{game_id}")
+async def run_all_steps(game_id: str):
 
     runner = active_games.get(game_id)
     if not runner:
         raise HTTPException(status_code=404, detail="Game not found")
 
-    state_snapshot = runner.run_next_step()
+    game_states = runner.run_all_step()
     
     game_state = {
         "game_id": game_id,
-        "step": runner.current_step,
-        "state": runner.game_state_manager.game_states,
+        "history": runner.game_state_manager.game_states,
     }
 
     # Store in Redis
     r.set(game_id, json.dumps(game_state))  # Serialize game_state to store in Redis
 
     return state_snapshot
-    
+
 @app.get("/api/game/{game_id}")
-async def game_step(game_id: str):
+async def get_latest_state(game_id: str):
 
     runner = active_games.get(game_id)
     if not runner:
         raise HTTPException(status_code=404, detail="Game not found")
     
-    state_snapshot = runner.game_state_manager.game_states
+    game_state_raw = r.get(game_id)
+    game_state.loads(game_state_raw)
     
-    return state_snapshot
+    return game_state.state
 
 @app.delete("/api/game/{game_id}")
 async def end_game(game_id: str):
@@ -100,7 +124,7 @@ async def end_game(game_id: str):
         r.delete(game_id)
         return {"message": "Game ended successfully"}
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Game not found")
+        raise HTTPException(status_code=404, detail="Game not found")
         
 @app.get("/")
 def read_root():
