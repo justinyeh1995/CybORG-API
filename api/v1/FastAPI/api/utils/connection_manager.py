3e6417c5e09f2992d8e64c29bfd2d713d1b73043
justinyeh1995 <justinyeh1995@gmail.com>
2024-10-19 18:26:23 -0500

    Debug disconnect

--- api/v1/FastAPI/api/utils/connection_manager.py
+++ api/v1/FastAPI/api/utils/connection_manager.py
@@ -1,24 +1,24 @@
-from api.v1.CybORG.CybORG.CyborgAAS.Runner.SimpleAgentRunner import SimpleAgentRunner
-from fastapi import WebSocket, WebSocketDisconnect
+from typing import Dict
+from fastapi import WebSocket 
 
 class WebSocketConnectionManager:
     def __init__(self):
-        self.active_connections: dict[str, WebSocket] = {}
+        self.active_connections: Dict[str, WebSocket] = {}
 
     async def connect(self, game_id: str, websocket: WebSocket):
         await websocket.accept()
         self.active_connections[game_id] = websocket
 
     def disconnect(self, game_id: str, websocket: WebSocket):
-        del self.active_connections[game_id]
+        if game_id in self.active_connections:
+            del self.active_connections[game_id]
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    async def send_personal_message(self, message: str, simulation_id: str):
+        websockets = self.active_connections.get(simulation_id, [])
+        for connection in websockets:
+            await connection.send_text(message)
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
             
-class ActiveGameManager:
-    def __init__(self):
-        self.active_games: dict[str, SimpleAgentRunner] = {}
