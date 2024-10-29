2a136b48b8f35130a78c0012e4e0d7e9277b2683
justinyeh1995 <justinyeh1995@gmail.com>
2024-10-29 18:46:14 -0500

    debug import route

--- api/v1/FastAPI/api/core/security.py
+++ api/v1/FastAPI/api/core/security.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta, timezone
-from typing import Any
+from typing import Any, Union
 
 import jwt
 from passlib.context import CryptContext
 
-from app.core.config import settings
+from FastAPI.api.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -12,9 +12,13 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ALGORITHM = "HS256"
 
 
-def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
-    expire = datetime.now(timezone.utc) + expires_delta
-    to_encode = {"exp": expire, "sub": str(subject)}
+def create_access_token(data: dict| Any,  expires_delta: Union[timedelta, None] = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
