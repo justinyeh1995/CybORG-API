40d260abad7c59be4dbd8f4c94d1e173a7e6948a
justinyeh1995 <justinyeh1995@gmail.com>
2024-11-06 14:55:00 -0600

    Adding create user endpoint

--- api/v1/FastAPI/crud_user.py
+++ api/v1/FastAPI/crud_user.py
@@ -1,11 +1,27 @@
 from http.client import HTTPException
 from sqlalchemy import func
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from api.v1.FastAPI import models, schemas
+from api.v1.FastAPI.schemas import User
+from api.v1.FastAPI.api.core.security import get_password_hash
+import uuid
+import logging
 
-def create_user(username, password, db):
-    pass
+def create_user(db_session: Session, user_in: schemas.UserCreate) -> User:
+    hashed_password = get_password_hash(user_in.password)
+    user = models.User(user_id=str(uuid.uuid4()),
+                email=user_in.email, 
+                full_name=user_in.full_name, 
+                hashed_password=hashed_password,
+                is_active=user_in.is_active,
+                is_superuser=user_in.is_superuser)
+    
+    db_session.add(user)
+    db_session.commit()
+    db_session.refresh(user)
+    return user
 
 def update_user(username, db):
     pass
@@ -13,8 +29,10 @@ def update_user(username, db):
 def delete_user(username, db):
     pass
 
-def get_user_by_email(email, db):
-    pass
+def get_user_by_email(*, session: Session, email: str) -> User | None:
+    statement = select(User).where(User.email == email)
+    session_user = session.exec(statement).first()
+    return session_user
 
 def get_user_by_username(username, db):
     pass
