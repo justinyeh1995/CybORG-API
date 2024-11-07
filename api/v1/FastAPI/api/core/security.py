c729457e51c10dc2835529a6520d2526ac0e91c7
justinyeh1995 <justinyeh1995@gmail.com>
2024-11-07 13:49:32 -0600

    rewrite create_access_token function

--- api/v1/FastAPI/api/core/security.py
+++ api/v1/FastAPI/api/core/security.py
@@ -12,13 +12,12 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ALGORITHM = "HS256"
 
 
-def create_access_token(data: dict| Any,  expires_delta: Union[timedelta, None] = None) -> str:
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
+def create_access_token(subject: str,  expires_delta: Union[timedelta, None] = None) -> str:
+    """
+    return a token that encapsulates the subject and expires time
+    """
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode = {"exp": expire, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
